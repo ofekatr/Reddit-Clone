48852f21715a5d0b583e15d0c8b2e036b757543f
Ofek Atar <ofek.atr@gmail.com>
2020-11-10 13:41:14 +0200

    Initialized mikro-orm, Initialized graphql, Implemented CRUD for posts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,38 @@
-console.log("Hello World");
+require('dotenv').config();
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import "reflect-metadata";
+import { MikroORM } from "@mikro-orm/core";
+
+import mikroConfig from "./mikro-orm.config";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
+import { PostsResolver } from "./resolvers/posts";
+
+const main = async () => {
+    const port = process.env.PORT || 3005;
+
+    const orm = await MikroORM.init(mikroConfig);
+    await orm.getMigrator().up();
+
+    const app = express();
+
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            validate: false,
+            resolvers: [HelloResolver, PostsResolver],
+        }),
+        context: () => ({ em: orm.em })
+    });
+
+    apolloServer.applyMiddleware({ app });
+
+    app.listen(port, () => {
+        console.log(`Server is running on port ${port}`);
+    });
+
+}
+
+main();
+
+export { };
